187af3edde5ca48a8df7053b3c64ffe373ce7922
YoungEun <y_e_99@ewhain.net>
2023-11-10 11:48:06 +0900

    add: gitignore file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
