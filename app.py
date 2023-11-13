562a9260a50ca63d9c24f19e250001ddf994fa59
haeun228 <123haeun@naver.com>
2023-11-14 00:23:09 +0900

    과제2

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from database import DBhandler
+import hashlib
 import sys
+
 application = Flask(__name__)
+application.config["SECRET_KEY"] = "helloosp"
+
+DB = DBhandler()
 
 @application.route("/")
 def hello():
     return render_template("index.html")
+
+@application.route("/login")
+def login():
+    return render_template("login.html")
+
+@application.route("/join")
+def join():
+    return render_template("join.html")
+
+@application.route("/join_post", methods=['POST'])
+def register_user():
+    data=request.form
+    pw=request.form['password']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.insert_user(data,pw_hash):
+        return render_template("login.html")
+    else:
+        flash("user id already exist!")
+        return render_template("join.html")
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
