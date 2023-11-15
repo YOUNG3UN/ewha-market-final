d11f4e4936d4635f60a7b962ed946edfa54af69d
nzeong <stac_cato@naver.com>
2023-11-15 04:40:08 +0000

    fix: insert 오류 수정

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import json
 class DBhandler:
     def __init__(self ):
         with open('./authentication/firebase_auth.json') as f:
-            config=json.load(f )
+            config=json.load(f)
             
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
@@ -20,10 +20,6 @@ class DBhandler:
             "img_path": img_path
         }
     
-        # 기본키(new_product_key) index 알아서 생성해서 db에 넣어주기
-        new_product_ref = self.db.child("product").push(product_info)
-        new_product_key = new_product_ref.key()
-        print("New product key:", new_product_key)
-        self.db.child("product").child(new_product_key).update(product_info)
+        self.db.child("product").push(product_info)
         
         return True
